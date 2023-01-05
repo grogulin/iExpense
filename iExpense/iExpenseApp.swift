7b83838de30c9437ab80bc8bf0818dddce7038be
Yaroslav Grogul <time7bass@gmail.com>
2023-01-05 17:28:42 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ярослав Грогуль on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
